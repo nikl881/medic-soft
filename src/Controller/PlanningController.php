cf414b6d3427fff7fa751d9e1dbb1eb7b9105f14
nikl881 <info@findniels.com>
2021-02-03 11:07:04 +0100

    :tada: initial commit

---
+++ src/Controller/PlanningController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlanningController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/planning.html.twig', [
+            'controller_name' => 'PlanningController',
+        ]);
+    }
+}
