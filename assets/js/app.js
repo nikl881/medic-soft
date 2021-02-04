2d0bfcb54fbada43e2022c902a5ef78d18adae5a
nikl881 <info@findniels.com>
2021-02-04 11:12:54 +0100

    :sparkles: fixed minimizer toggle CoreUI + setup webpack encore route

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
-import 'bootstrap';
-import '../styles/app.css';
+// Import CoreUI + dependency library
+
+
+// Import stylesheets
+require('/assets/styles/app.css');
+
+import '@coreui/coreui';
+import '@popperjs/core';
+
+
+// Import test ReactJS
 import '../js/react/reactIndex';
 
 
+// Import additional libraries
 
