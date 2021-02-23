957b0c431a5437b2141b9f1420a51f3397d06507
nikl881 <info@findniels.com>
2021-02-23 09:54:28 +0100

    :construction: patient detailpage

--- src/Form/AddPatientGeneralNoteType.php
+++ src/Form/AddPatientGeneralNoteType.php
@@ -5,28 +5,21 @@ namespace App\Form;
 
 use App\Entity\Patient;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AddPatientType extends AbstractType
+
+class AddPatientGeneralNoteType extends AbstractType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('lastName', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('phoneNumber', TelType::class)
-            ->add('insuranceNumber', NumberType::class)
-            ->add('insuranceCompany', TextType::class)
-            ->add('birthdate', BirthdayType::class)
+            ->add('generalNotes', TextType::class, [
+                'attr' => array('style' => 'height: 100px')
+            ])
             ->add('save', SubmitType::class);
     }
 
