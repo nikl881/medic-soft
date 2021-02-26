ca349ce799f5c9da079741cc768f91ba49aeaf31
nikl881 <info@findniels.com>
2021-02-26 16:28:29 +0100

    :construction: WIP AWS s3 image bucket for patient profile picture

--- src/Form/UpdateProfileImagePatientType.php
+++ src/Form/UpdateProfileImagePatientType.php
@@ -1,14 +1,11 @@
 <?php
 
-
 namespace App\Form;
 
-use App\Entity\Patient;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 
 class UpdateProfileImagePatientType extends AbstractType
@@ -29,10 +26,4 @@ class UpdateProfileImagePatientType extends AbstractType
             ->add('save', SubmitType::class);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Patient::class,
-        ]);
-    }
 }
